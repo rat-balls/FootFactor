db41231cad537773d815cc1be3d2df8e0c7bb906
Paul Rivallin <99149335+Roronoatii@users.noreply.github.com>
2025-09-05 20:51:20 +0200

    mob movement

--- Enemy/enemy.gd
+++ Enemy/enemy.gd
@@ -1,4 +1,10 @@
 extends CharacterBody2D
 
-@export var movement_speed = 20.0
+@export var movement_speed = 100.0
 	
+@onready var player = get_tree().get_first_node_in_group("player")
+
+func _physics_process(delta: float) -> void:
+	var direction = global_position.direction_to(player.global_position)
+	velocity = direction * movement_speed
+	move_and_slide()
--- Player/player.gd
+++ Player/player.gd
@@ -10,5 +10,5 @@ func movement():
 	var x_mov = Input.get_action_strength("Right") - Input.get_action_strength("Left")
 	var y_mov = Input.get_action_strength("Down") - Input.get_action_strength("Up")
 	var mov = Vector2(x_mov, y_mov)
-	velocity = mov.normalized() + movement_speed
+	velocity = mov.normalized() * movement_speed
 	move_and_slide()
