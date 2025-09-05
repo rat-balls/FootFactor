12464d27950bfa74e6ca7996651d375673cbd05c
rat-balls <delalande.ethan@gmail.com>
2025-09-05 13:48:31 +0200

    Added delta to character movement

--- Scripts/character_movement.gd
+++ Scripts/character_movement.gd
@@ -1,12 +1,12 @@
 extends Node2D
 
-@export var SPEED: float = 10.0
+@export var SPEED: float = 300.0
 
 func _process(delta):
 	
-	var dir: Vector2 = get_input()
+	var dir: Vector2 = get_input(delta)
 	translate(dir)
-	
-func get_input() -> Vector2:
+
+func get_input(delta: float) -> Vector2:
 	var input_direction: Vector2 = Input.get_vector("Left", "Right", "Up", "Down")
-	return input_direction * SPEED
+	return input_direction * SPEED * delta
