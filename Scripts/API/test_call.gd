3c07ff63c92fcb2f1896223a0bf6262437b8ab69
rat-balls <delalande.ethan@gmail.com>
2025-09-08 15:53:12 +0200

    Added websocket from JeuBoire

--- Scripts/API/test_call.gd
+++ Scripts/API/test_call.gd
@@ -7,5 +7,5 @@ func _ready():
 	http_request.request("https://foot-factor.onrender.com/")
 
 func _on_request_completed(result, response_code, headers, body):
-	if(result == 0): #0 is Result.RESULT_SUCCESS in the enum but idk how to access it yet
+	if(result == 0 ): #0 is Result.RESULT_SUCCESS in the enum but idk how to access it yet
 		print(body.get_string_from_utf8())
