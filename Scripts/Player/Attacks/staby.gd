78ff43719566eca2ff2c33c75024e683782731d7
rat-balls <delalande.ethan@gmail.com>
2025-09-15 13:16:28 +0200

    Added spider spritesheet, fixed typo, and enabled ETC2 ASTC texture for mac export

--- Scripts/Player/Attacks/staby.gd
+++ Scripts/Player/Attacks/staby.gd
@@ -33,21 +33,21 @@ func update_staby():
 			damage = 10
 			knockback_amount = 100
 			attack_size = 1.0 * (1 + player.spell_size)
-			attack_speed = 5.0 * (1 - player.spell_cooldow)
+			attack_speed = 5.0 * (1 - player.spell_cooldown)
 		2:
 			hp = 9999
 			speed = 200.0
 			damage = 10
 			knockback_amount = 100
 			attack_size = 1.0 * (1 + player.spell_size)
-			attack_speed = 5.0 * (1 - player.spell_cooldow)
+			attack_speed = 5.0 * (1 - player.spell_cooldown)
 		3:
 			hp = 9999
 			speed = 200.0
 			damage = 15
 			knockback_amount = 120
 			attack_size = 1.0 * (1 + player.spell_size)
-			attack_speed = 5.0 * (1 - player.spell_cooldow)
+			attack_speed = 5.0 * (1 - player.spell_cooldown)
 	
 	scale = Vector2(1.0, 1.0) * attack_size
 	attack_timer.wait_time = attack_speed
