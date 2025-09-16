c1c5884fea7d8e59b04bb584d17b5e4a97a48580
E.Delalande <116553314+rat-balls@users.noreply.github.com>
2025-09-16 13:31:39 +0200

    Added menu button to death_screen

--- Scripts/Player/player.gd
+++ Scripts/Player/player.gd
@@ -405,3 +405,8 @@ func adjust_ui_collection(upgrade):
 func _on_restart_button_button_up() -> void:
 	get_tree().paused = false
 	get_tree().reload_current_scene()
+
+
+func _on_menu_button_button_up() -> void:
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://Scenes/TitleScreen/menu.tscn")
